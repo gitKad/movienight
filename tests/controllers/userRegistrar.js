5059a2821bd2a25756a4b56742aeee0bf811a42e
gitKad <alexis.philippe@gmail.com>
2016-03-26 16:55:43 -0400

    user registrar will not duplicate accounts if it was previously lurked by my flixster lurker

--- tests/controllers/userRegistrar.js
+++ tests/controllers/userRegistrar.js
@@ -6,16 +6,47 @@ var expect = require('chai').expect,
 
 describe('My user Registrar', function() {
 
-  beforeEach(function(done){
+  before(function(done){
     userRegistrar = new userRegistrar();
     flixsterLurker = new flixsterLurker();
     done();
   });
 
+  beforeEach(function(done){
+    var barbu = {
+      profile: {
+        firstName: 'Mathieu',
+        lastName: 'Hébert'
+      },
+      accounts: {
+        flixster: 880813768
+      }
+    };
+    User.create(barbu,function(err,doc){
+      done();
+    });
+  });
+
   it('will save a never before seen user lurked by my flixster lurker',function(done) {
     User.count({},function(err,c){
-      expect(c).to.equal(0);
+      expect(c).to.equal(1);
       flixsterLurker.getFlixsterUsersScores(789760392,1,function(result){
+        var jsonResult = JSON.parse(result);
+        userRegistrar.registerFlixsterUserFromMovieRatings(jsonResult[0],function(aUser){
+          User.count({},function(err,c){
+            expect(c).to.equal(2);
+            expect(aUser).to.have.deep.property('accounts.flixster',jsonResult[0].user.id)
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  it('will not duplicate accounts of previously lurked users by my flixster lurker',function(done){
+    User.count({},function(err,c){
+      expect(c).to.equal(1);
+      flixsterLurker.getFlixsterUsersScores(880813768,1,function(result){
         var jsonResult = JSON.parse(result);
         userRegistrar.registerFlixsterUserFromMovieRatings(jsonResult[0],function(aUser){
           User.count({},function(err,c){
@@ -27,7 +58,5 @@ describe('My user Registrar', function() {
       });
     });
   });
-
-  it.skip('will not duplicate accounts of previously lurked users by my flixster lurker');
 
 });
