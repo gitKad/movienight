ec22d08ee0aea254ab86ebb45f0b20efdaef374a
gitKad <alexis.philippe@gmail.com>
2016-04-03 16:34:12 -0400

    added next callback

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,17 @@
-var router = require('express').Router(),
+var express = require('express'),
+    router = express.Router(),
     flixsterLurker = require('../lurkers/flixster'),
     userRegistrar = require('../controllers/userRegistrar'),
     mongoose = require('mongoose'),
     User = require('../models/user');
 
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
   User.find({},function(err, docs) {
     res.json(docs);
   });
 });
 
-router.get('/signup/flixster/:flixsterId', function(req, res) {
+router.get('/signup/flixster/:flixsterId', function(req, res,next) {
   flixsterLurker = new flixsterLurker();
   flixsterLurker.getFlixsterUsersScores(req.params.flixsterId,1,function(response) {
     var jsonResponse = JSON.parse(response);
