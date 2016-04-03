e2600c8b66e3b6853692a4a96e7ce026a3369652
gitKad <alexis.philippe@gmail.com>
2016-04-03 15:56:42 -0400

    uses user api route definition

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ function makeServer() {
   var express = require('express'),
       app = express();
 
-  app.get('/', function (req, res) {
-    res.json({'version' : '0.0.1'}).status(200);
-  });
-
-  //app.use(require('./controllers/index.js'));
+  app.use(require('./controllers/index.js'));
 
   var server = app.listen(1337, function () {
     var port = server.address().port;
