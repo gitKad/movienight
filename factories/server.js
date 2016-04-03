ed73a828e0bd7d0a7f5a16b1e4f474e78be159d1
gitKad <alexis.philippe@gmail.com>
2016-04-03 16:42:33 -0400

    added standard express middlewares to express and basic error mgmt

--- factories/server.js
+++ factories/server.js
@@ -1,12 +1,40 @@
 function makeServer() {
 
-  var express = require('express'),
-      routes = require('../routes/index'),
-      users = require('../routes/users'),
-      app = express();
+  var express = require('express');
+  var path = require('path');
+  var logger = require('morgan');
+  var cookieParser = require('cookie-parser');
+  var bodyParser = require('body-parser');
+
+  var routes = require('../routes/index');
+  var users = require('../routes/users');
+
+  var app = express();
+
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 
   app.use('/', routes);
   app.use('/users', users);
+
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  // will print stacktrace
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
 
   var server = app.listen(1337, function () {
     var port = server.address().port;
