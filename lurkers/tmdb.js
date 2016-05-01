01a7fc4344c7776afbe5140f938cdbbfadc8c9d4
gitKad <alexis.philippe@gmail.com>
2016-05-01 01:40:12 -0400

    tmdb lurker now uses promises

--- lurkers/tmdb.js
+++ lurkers/tmdb.js
@@ -1,74 +1,79 @@
 var request = require('request-promise');
+var Promise = require('bluebird');
 
 var tmdbLurker = function() {
   this.key = process.env.movienight_tmdbapikey;
 };
 
-tmdbLurker.prototype.getMovie = function (tmdbMovieID,limit,cb) {
+tmdbLurker.prototype.getMovie = function (tmdbMovieID,limit) {
   var options = {
     url: 'https://api.themoviedb.org/3/movie/'+tmdbMovieID+'?api_key='+this.key
   };
-
-  request(options)
-    .then(function(body) {
-      cb(body);
-    });
+  return request(options);
 }
 
-tmdbLurker.prototype.getDirectors = function (tmdbMovieID,cb) {
-  var options = {
-    url: 'https://api.themoviedb.org/3/movie/'+tmdbMovieID+'/credits?api_key='+this.key
-  };
+tmdbLurker.prototype.getDirectors = function (tmdbMovieID) {
+  var tmdbLurker = this;
 
-  request(options).then(function(body) {
-    var jsonBody = JSON.parse(body);
-    var directors = jsonBody.crew.filter(function (el) {
-      return el.job == 'Director' & el.department == 'Directing';
+  return new Promise(function(resolve,reject) {
+    var options = {
+      url: 'https://api.themoviedb.org/3/movie/'+tmdbMovieID+'/credits?api_key='+tmdbLurker.key
+    };
+
+    request(options).then(function(body) {
+      var jsonBody = JSON.parse(body);
+      var directors = jsonBody.crew.filter(function (el) {
+        return el.job == 'Director' & el.department == 'Directing';
+      });
+      resolve(directors);
     });
-    cb(directors);
   });
 }
 
-tmdbLurker.prototype.searchMoviePagesByTitle = function (title,page,cb) {
+tmdbLurker.prototype.searchMoviePagesByTitle = function (title, page) {
   var options = {
     url: 'https://api.themoviedb.org/3/search/movie?api_key='+this.key+'&query='+escape(title)+'&page='+page
   };
+  return request(options);
+}
 
-  request(options).then(function(body) {
-    cb(body);
+tmdbLurker.prototype.searchMovieByTitle = function (title, limit) {
+  var tmdbLurker = this;
+
+  return new Promise(function(resolve,reject){
+    var i = 1;
+    var result = [];
+    var jsonBody;
+
+    (function appendPages(i) {
+      i = i || 1;
+      tmdbLurker.searchMoviePagesByTitle(title,i)
+      .then(function(response) {
+        jsonBody = JSON.parse(response);
+        Array.prototype.splice.apply(result, [1,0].concat(jsonBody.results));
+        result.splice(limit,result.length-limit);
+        if(result.length < limit && jsonBody.total_pages > i ) {
+          appendPages(++i);
+        } else {
+          resolve(result);
+        }
+      });
+    })();
   });
 }
 
-tmdbLurker.prototype.searchMovieByTitle = function (title,limit,cb) {
-
-  var i = 1;
-  var result = [];
-  var jsonBody;
+tmdbLurker.prototype.searchMovieByTitleAndYear = function (title,year,limit) {
   var tmdbLurker = this;
-
-  (function appendPages(i) {
-    i = i || 1;
-    tmdbLurker.searchMoviePagesByTitle(title,i,function(response) {
-      jsonBody = JSON.parse(response);
-      Array.prototype.splice.apply(result, [1,0].concat(jsonBody.results));
-      result.splice(limit,result.length-limit);
-      if(result.length < limit && jsonBody.total_pages > i ) {
-        appendPages(++i);
-      } else {
-        cb(result);
+  return new Promise(function(resolve,reject){
+    tmdbLurker.searchMovieByTitle(title,limit)
+    .then(function(response) {
+      for (var i = 0; i < response.length; i++) {
+        if(response[i].release_date.substring(0,4) == year) {
+          resolve(response[i]);
+          break;
+        }
       }
     });
-  })();
-}
-
-tmdbLurker.prototype.searchMovieByTitleAndYear = function (title,year,limit,cb) {
-  this.searchMovieByTitle(title,limit,function(response) {
-    for (var i = 0; i < response.length; i++) {
-      if(response[i].release_date.substring(0,4) == year) {
-        cb(response[i]);
-        break;
-      }
-    }
   });
 }
 
--- tests/lurkers/tmdb.js
+++ tests/lurkers/tmdb.js
@@ -1,5 +1,5 @@
-var expect = require('chai').expect,
-    tmdbLurker = require('../../lurkers/tmdb');
+var expect = require('chai').expect;
+var tmdbLurker = require('../../lurkers/tmdb');
 
 describe('My themoviedb lurker',function() {
 
@@ -9,7 +9,8 @@ describe('My themoviedb lurker',function() {
   });
 
   it('can retrieve a movie from its TMDb id', function(done) {
-    tmdbLurker.getMovie(550,1,function(response){
+    tmdbLurker.getMovie(550,1)
+    .then(function(response) {
       var jsonResponse = JSON.parse(response);
       expect(jsonResponse).to.have.property('title','Fight Club');
       done();
@@ -17,7 +18,8 @@ describe('My themoviedb lurker',function() {
   });
 
   it('can browse first page of search results',function(done){
-    tmdbLurker.searchMoviePagesByTitle('test',1, function(response) {
+    tmdbLurker.searchMoviePagesByTitle('test',1)
+    .then(function(response) {
       var jsonResponse = JSON.parse(response);
       expect(jsonResponse).to.have.property('page',1);
       expect(jsonResponse).to.have.property('total_results');
@@ -27,7 +29,8 @@ describe('My themoviedb lurker',function() {
   });
 
   it('can browse second page of search results',function(done) {
-    tmdbLurker.searchMoviePagesByTitle('test', 2, function(response) {
+    tmdbLurker.searchMoviePagesByTitle('test', 2)
+    .then(function(response) {
       var jsonResponse = JSON.parse(response);
       expect(jsonResponse).to.have.property('page',2);
       expect(jsonResponse).to.have.property('total_results');
@@ -36,16 +39,18 @@ describe('My themoviedb lurker',function() {
     });
   });
 
-  it('can return multiple pages of results',function(done) {
+  it('can return multiple pages of results', function(done) {
     var numberOfItems = 21;
-    tmdbLurker.searchMovieByTitle('test', numberOfItems, function(response) {
+    tmdbLurker.searchMovieByTitle('test', numberOfItems)
+    .then(function(response) {
       expect(response).to.have.lengthOf(numberOfItems);
       done();
     });
   });
 
   it('can search movies by title and year',function(done) {
-    tmdbLurker.searchMovieByTitleAndYear('Test',2014,5,function(response) {
+    tmdbLurker.searchMovieByTitleAndYear('Test',2014,5)
+    .then(function(response) {
       var release_year = response.release_date.substring(0,4);
       expect(response).to.have.property('title','Test');
       expect(response).to.have.property('release_date');
@@ -55,7 +60,8 @@ describe('My themoviedb lurker',function() {
   });
 
   it('can retrieve directors of a movie',function(done) {
-    tmdbLurker.getDirectors(603,function(directors) {
+    tmdbLurker.getDirectors(603)
+    .then(function(directors) {
       expect(directors).to.have.lengthOf(2);
       expect(directors[0]).to.have.property('name');
       expect(directors[1]).to.have.property('name');
